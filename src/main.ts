d2ecdf17169c22944d83e6dbb1b9754376f91cf7
milkWay <745309089@qq.com>
2021-03-23 22:21:37 +0800

    tsconfig

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.scss'
 import {createWebHashHistory,createRouter} from 'vue-router'
-
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
