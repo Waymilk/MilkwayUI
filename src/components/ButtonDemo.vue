b9dbc01a14646f9f40858e90592062384236d844
milkWay <745309089@qq.com>
2021-03-24 23:17:09 +0800

    Button点击事件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,7 @@
   <div>Button示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small">你好</Button>
   </div>
 </template>
 <script>
@@ -10,6 +10,12 @@ import Button from '../lib/Button.vue'
 export default {
   components:{
     Button
+  },
+  setup(props) {
+    const onClick = ()=>{
+      console.log(1233);
+    }
+    return {onClick}
   }
 }
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="toggle" class="m-switch" :class="{'z-checked' : value}"><span></span></button>
+    <button @click="toggle" class="milk-switch" :class="{'z-checked' : value}"><span></span></button>
   </div>
 </template>
 <script lang="ts">
@@ -19,47 +19,47 @@ export default{
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss" >
 $height: 20px;
 $width: 40px;
-  .m-switch {
-    cursor: pointer;
-    height: $height;
-    min-width: $width;
-    border: none;
-    background: #e3e3e3;
-    border-radius: $height /2;
-    position: relative;
-    margin: 0;
-    padding: 0;
-    outline: none;
+.milk-switch {
+  cursor: pointer;
+  height: $height;
+  min-width: $width;
+  border: none;
+  background: #e3e3e3;
+  border-radius: $height /2;
+  position: relative;
+  margin: 0;
+  padding: 0;
+  outline: none;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 1px;
+    height: 16px;
+    width: 16px;
+    background: #fdfdfd;
+    border-radius: 100%;
+    transition: all .3s;
+  }
+  &.z-checked {
+    background: #7e92da;
+  }
+  &.z-checked > span {
+    left: 100%;
+    margin-left: -1px;
+    transform: translateX(-100%);
+  }
+  &:active {
     > span {
-      position: absolute;
-      top: 2px;
-      left: 1px;
-      height: 16px;
-      width: 16px;
-      background: #fdfdfd;
-      border-radius: 100%;
-      transition: all .3s;
-    }
-    &.z-checked {
-      background: #7e92da;
-    }
-    &.z-checked > span {
-      left: 100%;
-      margin-left: -1px;
-      transform: translateX(-100%);
-    }
-    &:active {
-      > span {
-        width: $height + 2px;
-      }
-    }
-    &.z-checked:active {
-      > span {
-        width: $height + 2px;
-      }
+      width: $height + 2px;
     }
   }
+  &.z-checked:active {
+    > span {
+      width: $height + 2px;
+    }
+  }
+}
 </style>
