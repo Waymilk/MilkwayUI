2fff7d87b90cb124ed6ae8bb97a51bca7516f086
milkWay <745309089@qq.com>
2021-04-07 21:45:20 +0800

    优化Markdown

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import App from './App.vue'
 import './index.scss'
 import {createWebHashHistory,createRouter} from 'vue-router'
 import IconSvg from './components/Icon.vue'
+// import Markdown from './components/Markdown.vue'
 import 'github-markdown-css'
 const app = createApp(App)
 app.component('icon-svg',IconSvg)
+// app.component('Markdown',Markdown)
 app.use(router)
 app.mount('#app')
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 /*
- * @Descripttion: 
+ * @Descripttion:
  * @Author: Milkway
  * @Date: 2021-03-23 16:13:22
  * @LastEditors: Milkway
@@ -16,15 +16,18 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import Markdown from './components/Markdown.vue'
+import { h } from 'vue'
+const md = filname => h(Markdown,{path:`../markdown/${filname}.md`,key:filname})
 
 export const router  = createRouter({
   history,
   routes:[
     {path:'/',component: Home},
     {path:'/doc',component: Doc,children:[
-      {path:'intro',component: Intro},
-      {path:'install',component: Install},
-      {path:'get-started',component: GetStarted},
+      {path:'intro',component: md('intro')},
+      {path:'install',component: md('install')},
+      {path:'get-started',component: md('get-started')},
       {path:'switch',component: SwitchDemo},
       {path:'button',component: ButtonDemo},
       {path:'dialog',component: DialogDemo},
